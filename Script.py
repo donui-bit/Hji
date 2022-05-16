0da91061e301b1991e9d8c8cdcb2b6fd29fe8156
donui-bit <68704259+donui-bit@users.noreply.github.com>
2022-05-16 07:05:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """ʜᴇʟᴏ {},
-ᴍʏ ɴᴀᴍᴇ ɪs<a href=https://t.me/{}>{}</a>,ᴛʜɪs ʙᴏᴛ ᴍᴀᴅᴇ ғᴏʀ<a href=https://t.me/MOVIE_LOKHAM>ᴍᴏᴠɪᴇ 🍿 ʟᴏᴋʜᴀᴍ</a>"""
+ᴍʏ ɴᴀᴍᴇ ɪs <a href=https://t.me/{}>{}</a>,ᴛʜɪs ʙᴏᴛ ᴍᴀᴅᴇᴅ ғᴏʀ <a href=https://t.me/MOVIE_LOKHAM>ᴍᴏᴠɪᴇ 🍿 ʟᴏᴋʜᴀᴍ</a>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
