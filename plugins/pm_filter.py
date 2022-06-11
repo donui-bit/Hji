5eb0388b56a2520282f0303e19d13afe2a4ec3fe
donui-bit <68704259+donui-bit@users.noreply.github.com>
2022-06-11 18:03:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -753,7 +753,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply("ഗൂഗിളിൽ പോയി കറക്റ്റ് സ്പെല്ലിങ് ടൈപ്പ് ചെയ്യൂ ")
         await asyncio.sleep(8)
         await k.delete()
         return
